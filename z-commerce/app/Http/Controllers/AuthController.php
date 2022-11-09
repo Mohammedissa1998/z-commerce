5ad5e52c3331748331844c9eb7b266ac44c212f9
Mohammedissa1998 <mohammed_eissa-lb@outlook.com>
2022-11-09 15:38:18 +0300

    login and register page

---
+++ z-commerce/app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //show login page
+    public function showLogin()
+    {
+        return view('pages.login');
+    }
+
+    //show register page
+    public function showRegister()
+    {
+        return view('pages.register');
+    }
+
+     //register user
+
+     
+    //login user
+
+    
+}
---
+++ z-commerce/resources/views/pages/register.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('content')
+
+    register page
+@endsection
