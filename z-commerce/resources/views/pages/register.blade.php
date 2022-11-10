d5a259cec9a4da7bb098ed1602c0aaf71266b431
Mohammedissa1998 <mohammed_eissa-lb@outlook.com>
2022-11-10 16:03:35 +0300

    creating register page

--- z-commerce/resources/views/pages/register.blade.php
+++ z-commerce/resources/views/pages/register.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.master')
 @section('content')
+@section('title', 'Register')
 
-    register page
+    <section class="login-page"> 
+        <div class="login-form-box">
+            <div class="login-title"> Register</div>
+            <div class="login-form">
+                <form action="" method="">
+
+                    <div class="field">
+                        <label for="name">name</label>
+                        <input type="text" id="name" name="name" placeholder="john" >
+
+                    </div>
+
+                    <div class="field">
+                        <label for="email">email</label>
+                        <input type="email" id="email" name="name" placeholder="john@gmail.com" >
+
+                    </div>
+
+                    <div class="field">
+                        <label for="password">password</label>
+                        <input type="password" id="password" name="password" placeholder="******" >
+
+                    </div>
+
+                    <div class="field">
+                        <label for="passowrd_confirmation">confirm password</label>
+                        <input type="password" id="password" name="passowrd_confirmation" placeholder="******" >
+
+                    </div>
+
+                    <div class="field">
+                        <button type= "submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+
+    </section>
 @endsection
